dc9b67ab430e2a266442421417aa486c91e9777c
ahmed maroof <ahmedmaroof@ibm.com>
2022-01-28 00:13:49 +0200

    feedback endpoints cont.

--- feedbackMS/src/main/java/com/ofa/feedbackMS/controller/FeedBackController.java
+++ feedbackMS/src/main/java/com/ofa/feedbackMS/controller/FeedBackController.java
@@ -1,15 +1,34 @@
 package com.ofa.feedbackMS.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ofa.feedbackMS.model.Feedback;
+import com.ofa.feedbackMS.service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/feedbacks")
 public class FeedBackController {
 
+    @Autowired
+    FeedbackService feedbackService;
+
     @GetMapping("/")
     public String health() {
         return "FeedBack is Ok";
+    }
+
+    @GetMapping("/feedback/{id}")
+    public ResponseEntity<List<Feedback>> getFeedBacks(@PathVariable("id") String customerId) {
+        return new ResponseEntity<List<Feedback>>(feedbackService.getAllOrdersByCustomerId(Integer.valueOf(customerId).intValue()), HttpStatus.OK);
+    }
+
+
+    @PostMapping("/feedback")
+    private Feedback saveFeedback(@RequestBody Feedback feedback) {
+        return feedbackService.saveOrUpdate(feedback);
     }
 }

c7d5288d346e8e8544c55bf65cbf44b362e21a37
ahmed maroof <ahmedmaroof@ibm.com>
2022-01-27 14:51:54 +0200

    consolidation & configuration server  microservice

---
+++ consolidateMS/src/main/java/com/ofa/consolidateMS/ConsolidateMsApplication.java
@@ -0,0 +1,13 @@
+package com.ofa.consolidateMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolidateMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsolidateMsApplication.class, args);
+	}
+
+}
